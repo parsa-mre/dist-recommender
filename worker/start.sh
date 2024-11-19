57a2c40f6a7e3b2a8f0bd840caf3c1c9e5835e79
parsamre <p.mre@yahoo.com>
2024-11-18 22:16:31 -0500

    set log level to debug

--- worker/start.sh
+++ worker/start.sh
@@ -3,7 +3,7 @@
 cd worker_app
 # Start Celery worker
 celery -A tasks worker \
-    --loglevel=INFO \
+    --loglevel=DEBUG \
     --concurrency="${CELERY_WORKERS}" \
     --max-tasks-per-child="${CELERY_MAX_TASKS_PER_CHILD}" \
     --time-limit="${CELERY_TASK_TIME_LIMIT}" \
